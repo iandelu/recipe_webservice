fc443211b441a643f7f29f0ab37267f967551526
Anedell <48674592+iandelu@users.noreply.github.com>
2024-03-26 00:24:43 +0100

    restructure

--- src/usdaScraping.py
+++ src/usdaScraping.py
@@ -4,7 +4,8 @@ from usdaFoodScrap.sql_commands import generate_sql_insert_commands
 
 api_key = 'bpiX1h0D33qOcFfgfzi1OTnIoUrg5B0fq8x6l4DO'
 url_base = 'https://api.nal.usda.gov/fdc/v1/foods/search'
-initial_page=1
+initial_page=30
+total_pages = 10
 
 params_base = {
     'query': 'raw',
@@ -16,8 +17,8 @@ params_base = {
 nombres_guardados = []
 
 def main():
-    total_pages = 15
-    foods, categories = fetch_and_process_foods(total_pages, url_base, api_key, params_base)
+    
+    foods, categories = fetch_and_process_foods(total_pages, url_base, api_key, params_base, initial_page)
     
     write_to_csv(foods)
     write_to_txt(categories, "sources/categories.txt")
@@ -27,6 +28,7 @@ def main():
     product_inserts, macronutrients_inserts = generate_sql_insert_commands(foods, product_table_name, macronutrients_table_name, nutrient_map)
     
     write_sql_commands_to_files(product_inserts, macronutrients_inserts)
+    print(str(len(foods)) + " foods added")
 
 if __name__ == "__main__":
     main()
